6186de929a0ec067479a693dc36f4ea58466dcfe
David S. <neojohn2312@gmail.com>
2024-10-21 14:38:12 -0500

    3rd-commit

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,2 +1,14 @@
 // Write your "actions" router here!
 
+const express = require("express");
+const Actions = require("./actions-model");
+
+const {
+  checkIdActions,
+  checkPostActions,
+  checkPutActions,
+} = require("./actions-middlware");
+
+const router = express.Router();
+
+// Get all actions
